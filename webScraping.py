b3d8a0fdb95649692285e434138b614e028e8ab0
semihsevik <67510007+semihsevik@users.noreply.github.com>
2020-09-17 00:43:33 +0300

    Add files via upload

---
+++ webScraping.py
@@ -0,0 +1,80 @@
+import requests 
+from bs4 import BeautifulSoup 
+
+#Siteye bir get isteği gönderip html içeriğini çekelim.
+url = "https://www.doviz.com/"
+response = requests.get(url)
+htmlContent = response.content 
+
+#Bir soup objesi oluşturup html ile parse etmesini isteyelim.
+soup = BeautifulSoup(htmlContent , "html.parser")
+
+#İsimleri ve değerleri 'span' etiketinden 'class' isimlerine göre çekelim.
+names = soup.find_all("span" , {"class" : "name"})
+values = soup.find_all("span" , {"class" : "value"})
+
+#Çektiğimiz verilerin text kısımlarını alarak 'döviz.txt' dosyasına yazalım.
+for name , value in zip(names , values):
+    name = name.text 
+    value = value.text 
+    with open("döviz.txt" , "a" , encoding="UTF-8") as file: 
+        file.write(f"{name} - {value}\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
